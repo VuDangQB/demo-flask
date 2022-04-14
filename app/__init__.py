b28b9abbf79af08df10322b023f6763726a6528e
vudq <dangquocvubk@gmail.com>
2022-04-14 23:25:22 +0700

    update code

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app(config_name="dev"):
     app = Flask(__name__)
 
-    from .apis import blueprint
+    from app.apis import blueprint
     # from app.views import main_blp
     app.register_blueprint(blueprint)
     return app
