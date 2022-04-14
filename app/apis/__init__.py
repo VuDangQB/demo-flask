1afe8e0053980a3b249f2a9bb281aa548b42f5d6
vudq <dangquocvubk@gmail.com>
2022-04-15 00:01:11 +0700

    fix some errors

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -1,7 +1,7 @@
 from flask import (
     Blueprint
 )
-blueprint = Blueprint('api', __name__, url_prefix='/api')
+blueprint = Blueprint('api', __name__, url_prefix='/')
 
 @blueprint.route('/')
 def hello():
