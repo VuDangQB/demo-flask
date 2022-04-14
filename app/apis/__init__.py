9c231b022bc5b99cc6990ae2c48375d5104944a2
vudq <dangquocvubk@gmail.com>
2022-04-14 18:14:26 +0700

    update

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -1,7 +1,7 @@
 from flask import (
     Blueprint
 )
-blueprint = Blueprint('api', __name__, url_prefix='/')
+blueprint = Blueprint('api', __name__, url_prefix='/api')
 
 @blueprint.route('/')
 def hello():
